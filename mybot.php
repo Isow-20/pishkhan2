8fa32712122e5a30fa9f86da7a81fe678eda89d9
Isow-20 <76471625+Isow-20@users.noreply.github.com>
2020-12-26 12:46:02 +0330

    Update mybot.php

--- mybot.php
+++ mybot.php
@@ -13,40 +13,26 @@ $chat = $message->chat;
 $user_id = $chat->id;
 
 
-
-function inlinekeyboardmessage ($user_id,$message,$token,$keyboard) {
+//Send Text Message With Inline Keyboard
+function bot_keyboardmessage ($user_id,$message,$token,$keyboard) {
 	$url = 'https://api.telegram.org/bot'.$token.'/sendMessage';
 	$replyMarkup = array(
-				'inline_keyboard' => $keyboard
-			);
-	$encodedMarkup = json_encode($replyMarkup);
-	$url = 'https://api.telegram.org/bot'.$token.'/sendMessage?chat_id='.$user_id.'&text='.$message.'&reply_markup='.$encodedMarkup;
-	$update = file_get_contents($url);
-}
-
-
-//Send Text Message With Inline Keyboard
-// function bot_keyboardmessage ($user_id,$message,$token,$keyboard) {
-	// $url = 'https://api.telegram.org/bot'.$token.'/sendMessage';
-	// $replyMarkup = array(
-				// 'keyboard' => $keyboard,
-				// 'resize_keyboard' => true);
-				// $encodedMarkup = json_encode($replyMarkup);
-				// $message = urlencode($message);
-				// $url = 'https://api.telegram.org/bot'.$token.'/sendMessage?chat_id='.$user_id.'&text='.$message.'&reply_markup='.$encodedMarkup;
-				// $update = file_get_contents($url);
+				'keyboard' => $keyboard,
+				'resize_keyboard' => true);
+				$encodedMarkup = json_encode($replyMarkup);
+				$message = urlencode($message);
+				$url = 'https://api.telegram.org/bot'.$token.'/sendMessage?chat_id='.$user_id.'&text='.$message.'&reply_markup='.$encodedMarkup;
+				$update = file_get_contents($url);
 			
-// }
+}
 if ($text == '/start'){
-	$message = "سلام !"."\r\n"."به پنهان چت خوشومدی\xF0\x9F\x98\x89"."\r\n"."چه کاری می تونم برات انجام بدم؟🥰";
-	$keyboard = array(	
-					array(
-						array( 'امتیاز من',"امتیاز های برتر"),
-						array( 'شارژ رایگان' , "راهنما"),
-						)
+	$message = "سلام !"."\r\n"."به پیشخوان خاکستری خوشومدی\xF0\x9F\x98\x89"."\r\n"."چه کاری می تونم برات انجام بدم؟🥰";
+	$keyboard = array(
+					array( 'امتیاز من',"امتیاز های برتر"),
+					array( 'شارژ رایگان' , "راهنما"),
 				);
 
-	inlinekeyboardmessage ($user_id,$message,$token,$keyboard);
+	bot_keyboardmessage ($user_id,$message,$token,$keyboard);
 
 }
 else {
